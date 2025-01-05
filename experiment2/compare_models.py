a02d9a8511a720c8620573809d81d8f6ccd4f03c
Tilman <tilmanburghoff@posteo.de>
2025-01-05 23:05:16 +0100

    metrics for model comparison

--- experiment2/compare_models.py
+++ experiment2/compare_models.py
@@ -1,4 +1,22 @@
 import numpy as np
 import pandas as pd
 from model_testing_interface import Linear, FullyConnected, Convolution
+from model_evaluation_metrics import mean_square_error, R_squared, error_standard_deviation, avg_rec_std
 
+models = {
+    "Linear": Linear(),
+    "2-Layer FC": FullyConnected(0),
+    "3-Layer FC": FullyConnected(1),
+    "4-Layer FC": FullyConnected(2),
+    "1-Channel CNN": Convolution(1),
+    "2-Channel CNN": Convolution(2),
+    "3-Channel CNN": Convolution(3),
+    "4-Channel CNN": Convolution(4)
+}
+
+metrics = {
+    "Mean Sqaure Error": mean_square_error,
+    "R squared": R_squared,
+    "Standard Deviation of Error": error_standard_deviation,
+    "Standard deviation per Recording (avg)": avg_rec_std
+}
---
+++ experiment2/model_evaluation_metrics.py
@@ -0,0 +1,18 @@
+import numpy as np
+
+def mean_square_error(y_true, y_pred):
+    return np.average((y_true - y_pred)**2)
+
+def R_squared(y_true, y_pred):
+    return 1 - np.sum((y_pred.numpy().ravel() - y_true)**2) / np.sum((y_true - np.mean(y_true))**2)
+
+def error_standard_deviation(y_true, y_pred):
+    return np.std(y_true, y_pred)
+
+def avg_rec_std(y_true, y_pred):
+    recordings = np.unique(y_true)
+    summed_std = 0
+    for rec in recordings:
+        indizes, = np.nonzero(y_true == rec)
+        summed_std += np.std(y_pred[indizes])
+    return summed_std / len(recordings)
