77ee8e90366120f00ae4de65df6edd10dc7df729
Tilman <tilmanburghoff@posteo.de>
2025-01-11 23:36:48 +0100

    new robot control scheme
    - implements stationary-automated.py, a version of
      stationary randomized which plays "catridges" (iterators)
      from robot_movement_iterators.
    - Todo: more iterators and a way to switch between them.

---
+++ data-recording-scripts/robot_movement_iterators.py
@@ -0,0 +1,46 @@
+import numpy as np
+import copy
+
+class Grid_2d:
+    def __init__(self):
+        # TODO better names
+        self.start_pos=np.array([-1.5708, 0, 0, -0.3, 0, 1.5708, 0])
+        self.joint_x=0
+        self.joint_y=3
+        self.points_x=32
+        self.points_y=25
+        self.step_x=0.1
+        self.step_y=-0.1
+        self.continue_from=0
+        self.public_variable_names = ['start_pos', 'joint_x', 'joint_y', 
+                                      'points_x', 'points_y', 'step_x', 
+                                      'step_y', 'continue_from']
+        
+    def get_variable_names(self):
+        return self.public_variable_names
+
+    def get_position_by_index(self, idx):
+        
+        y_mult = idx // self.points_x
+        x_mult = idx % self.points_x if y_mult % 2 == 0 else self.points_x -1 - (idx % self.points_x)
+        self.pose[self.joint_x] = self.start_pos[self.joint_x] + x_mult * self.step_x
+        self.pose[self.joint_y] = self.start_pos[self.joint_y] + y_mult * self.step_y
+        print(x_mult, y_mult)
+        return self.pose
+    
+
+    def get_iterator(self):
+        self.pose = copy.copy(self.start_pos)
+        def grid_2d_iter():
+            positions = self.points_x * self.points_y
+            if self.continue_from == 0:
+                yield self.start_pos, 5, False
+            else:
+                pose = self.get_position_by_index(self.continue_from - 1)
+                yield pose, 5, False
+            for i in range(self.continue_from, positions):
+                print(f"Moving to position_index {i}")
+                pose = self.get_position_by_index(i)
+                yield pose, 1, True
+
+        return grid_2d_iter()
---
+++ data-recording-scripts/stationary-automated.py
@@ -0,0 +1,128 @@
+import numpy as np
+import os
+import audio_recorder
+from ros_controller import ROSController
+from robot_movement_iterators import Grid_2d
+from time import sleep
+
+DEBUG = True # if Robot is not available
+
+class robotRecording:
+    def __init__(self, position_iterator, rec_length, wait_for_move=False, data_dir_path="./data", rng_seed=None, notes="", debug=False):
+        print("\n--- starting setup ---\n")
+        self.rec_length = rec_length
+        self.position_iterator = position_iterator
+        self.wait_for_move = wait_for_move
+
+        self.setup_data_dir(data_dir_path)
+        print("- folder setup complete")
+        self.notes = notes
+
+        # TODO
+        self.joint_limits_max = np.array([1,1,1,1,1,1,1])
+        self.joint_limits_min = np.array([-1,-1,-1,-1,-1,-1,-1])
+
+        self.rng = np.random.default_rng(rng_seed)
+
+        self.ros_controller = ROSController(debug=debug)
+        print("- ros setup complete")
+
+        self.audio_recorder = audio_recorder.AudioRecorder()
+        print("- audio setup complete")
+        print("\n--- setup complete ---\n")
+        print(f"starting session {self.session} at index {self.index}.")
+
+
+    def setup_data_dir(self, data_dir_path):
+        self.data_dir = os.path.normpath(data_dir_path)
+
+        if not os.path.exists(self.data_dir):
+            os.makedirs(self.data_dir)
+
+        self.sample_idx_file = os.path.join(self.data_dir, "samples.csv")
+        self.metadata_file = os.path.join(self.data_dir, "metadata.txt")
+
+        if not os.path.exists(self.sample_idx_file):
+            with open(self.sample_idx_file, "x") as f:
+                f.write("idx,q_0,q_1,q_2,q_3,q_4,q_5,q_6,samples,SR,session_id,notes\n")
+            
+            with open(self.metadata_file, "x") as f:
+                f.write("session: 0\nindex:0")
+            self.session = 0
+            self.index = 0
+
+        else:
+            with open(self.metadata_file, "r") as f:
+                metadata = f.readlines()
+            
+            self.session = int(metadata[0].split(":")[1]) + 1
+            self.index = int(metadata[1].split(":")[1]) + 1
+    
+    def record(self):        
+        for position, movetime, for_recording in self.position_iterator:
+            print(f"\nmoving robot to {np.round(position, 2)}")
+            self.ros_controller.move_to_position(position, movetime)
+
+            if self.wait_for_move:
+                sleep(movetime)
+            
+            if for_recording:
+                print("recording audio")
+                self.record_sample(position)
+
+            self.index += 1
+
+    def record_sample(self, joint_pos):
+        sound_file = os.path.join(self.data_dir, f"{self.index}.wav")
+        samples = self.audio_recorder.create_recording(self.rec_length, sound_file)
+
+        joint_txt = ", ".join([str(q) for q in joint_pos])
+
+        with open(self.sample_idx_file, "a") as f:
+            f.write(f"{self.index}, {joint_txt}, {samples}, {self.audio_recorder.SR}, {self.session}, {self.notes}\n")
+
+        with open(self.metadata_file, "w") as f:
+                f.write(f"session: {self.session}\nindex: {self.index}")
+        
+
+
+def select_catridge():
+    # TODO select from different movement iterators
+    selected = Grid_2d()
+    print("The Parameters of this Iterator are:")
+    parameterlist = selected.get_variable_names()
+    for i, name in enumerate(parameterlist):
+        print(f"{i:2}: {name} = {selected.__getattribute__(name)}")
+    
+    change_vars = True
+    while change_vars:
+        if var_idx := input("Index of variable to change (enter to finish): "):
+            name = parameterlist[int(var_idx)]
+            val = eval(input(f"{name} = "))
+            selected.__setattr__(name, val)
+        else:
+            change_vars = False
+    print("The values are now:")
+    for i, name in enumerate(selected.get_variable_names()):
+        print(f"{i:2}: {name} = {selected.__getattribute__(name)}")
+    return selected.get_iterator()
+
+
+
+if __name__ == "__main__":
+    if rec_length := input("Recording length in seconds: "):
+        rec_length = int(rec_length)
+    else:
+        rec_length = 5
+
+    if notes := input("Input notes: "):
+        rec_length = int(rec_length)
+    else:
+        notes = ""
+    
+    iterator = select_catridge()
+
+    record = robotRecording(iterator, rec_length, notes=notes, debug=DEBUG)
+    print("Stay clear of the workspace of the robot!")
+    input("Press enter to start recording.")
+    record.record()
