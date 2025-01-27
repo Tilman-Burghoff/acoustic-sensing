d6fc3200655dff64951a834375321984eb608f2b
Tilman <tilmanburghoff@posteo.de>
2025-01-27 20:10:06 +0100

    analysing rotation
    - data_utils_1d and model_testing_interface_1d copied from experiment 2

--- experiment3/data_utils_1d.py
+++ experiment3/data_utils_1d.py
@@ -1,5 +1,6 @@
 import numpy as np
 import scipy.io.wavfile
+import os
 import pandas as pd
 
 
@@ -7,18 +8,23 @@ def get_labels(path):
     return pd.read_csv(path, dtype={"notes":"str"})
 
 def read_data(path="./data", 
-            inputlength_s=4, 
+            inputlength_s=5, 
             sample_rate=16000, 
             outputlength_samples=2048, 
             normalize=False,
             apply_fft=True,
-            label_file="./data/samples.csv"):
+            label_file="./data/samples.csv",
+            label_column="q_0"):
     
     labels = get_labels(label_file)
 
+    inputfiles = os.listdir(path)
+
     split_into = (inputlength_s * sample_rate // outputlength_samples)
-    output_datapoints = (np.max(labels.idx)+1) * split_into
+    output_datapoints = len(inputfiles) * split_into
     req_inputlength = split_into * outputlength_samples
+
+    # X_long = np.zeros((0,80896))
 
     print(f"Splitting each input into {split_into} datapoints, resulting in {output_datapoints} samples")
 
@@ -26,28 +32,28 @@ def read_data(path="./data",
     X2 = np.zeros((output_datapoints, outputlength_samples))
     X3 = np.zeros((output_datapoints, outputlength_samples))
     X4 = np.zeros((output_datapoints, outputlength_samples))
+    y = np.full((output_datapoints, 2), np.nan)
 
-    y = np.repeat(np.hstack([np.arange(len(labels))[:,None], labels[["q_0", "q_3"]].to_numpy()]), split_into, axis=0)
-
-    for idx, row in labels.iterrows():
-
-        sr, data = scipy.io.wavfile.read(f"{path}/{row.idx}.wav")
+    for idx in labels.idx:
+        sr, data = scipy.io.wavfile.read(f"{path}/{idx}.wav")
         if sr != sample_rate:
-            raise(f"Samplerate of {row.idx}.wav is {sr} instead of {sample_rate}")
+            raise(f"Samplerate of {idx}.wav is {sr} instead of {sample_rate}")
         
         if len(data) < req_inputlength:
-            raise(f"File {row.idx}.wav is not long enough")
+            raise(f"File {idx}.wav is not long enough")
 
         # X_long = np.vstack((X_long, data[:,0]))
-        start_of_block = 16000 # remove first second
+        start_of_block = (len(data) - req_inputlength) // 2
         data_block1 = data[start_of_block:start_of_block+req_inputlength, 1]
         data_block2 = data[start_of_block:start_of_block+req_inputlength, 2]
         data_block3 = data[start_of_block:start_of_block+req_inputlength, 3]
         data_block4 = data[start_of_block:start_of_block+req_inputlength, 4]
-        X1[idx*split_into:(idx+1)*split_into, :] = data_block1.reshape((split_into, outputlength_samples))
-        X2[idx*split_into:(idx+1)*split_into, :] = 0.5*(data_block1 + data_block2).reshape((split_into, outputlength_samples))
-        X3[idx*split_into:(idx+1)*split_into, :] = 0.5*(data_block1 + data_block3).reshape((split_into, outputlength_samples))
-        X4[idx*split_into:(idx+1)*split_into, :] = 0.5*(data_block1 + data_block4).reshape((split_into, outputlength_samples))
+        i = idx
+        X1[i*split_into:(i+1)*split_into, :] = data_block1.reshape((split_into, outputlength_samples))
+        X2[i*split_into:(i+1)*split_into, :] = 0.5*(data_block1 + data_block2).reshape((split_into, outputlength_samples))
+        X3[i*split_into:(i+1)*split_into, :] = 0.5*(data_block1 + data_block3).reshape((split_into, outputlength_samples))
+        X4[i*split_into:(i+1)*split_into, :] = 0.5*(data_block1 + data_block4).reshape((split_into, outputlength_samples))
+        y[i*split_into:(i+1)*split_into, :] = np.array((idx, labels.loc[labels.idx==idx, label_column].item()))
 
     if normalize:
         X1 = X1 / np.max(np.abs(X1))
@@ -68,8 +74,7 @@ def read_data(path="./data",
 
 def k_fold_split(X, y, k_fold=5, seed=0):
     rng = np.random.default_rng(seed)
-    shuffeled_idxs = np.unique(y[:,0])
-    print(shuffeled_idxs[-1])
+    shuffeled_idxs = np.unique(y[:,0])[:-1] # remove nan
     rng.shuffle(shuffeled_idxs)
     block_length = len(shuffeled_idxs)//k_fold
     X_split = []
@@ -77,7 +82,7 @@ def k_fold_split(X, y, k_fold=5, seed=0):
     for i in range(k_fold):
         split_idxs, = np.nonzero(np.isin(y[:,0], shuffeled_idxs[i*block_length:(i+1)*block_length]))
         X_split.append(X[split_idxs,:,:])
-        y_split.append(y[split_idxs,1:])
+        y_split.append(y[split_idxs,1])
 
     return X_split, y_split
 
